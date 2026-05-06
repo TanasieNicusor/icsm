4fa892ce398de3280da7e2f847fa92860b5415b2
TanasieNicusor <tansie.cristian.g3c@student.ucv.ro>
2026-05-06 16:38:00 +0300

    web app foundation

--- src/main/java/com/example/icsm/controller/HomeController.java
+++ src/main/java/com/example/icsm/controller/HomeController.java
@@ -6,10 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    /**
-     * Maps the root URL to the home template.
-     * The view name "home/index" resolves to src/main/resources/templates/home/index.html
-     */
     @GetMapping("/")
     public String index() {
         return "home/index";
