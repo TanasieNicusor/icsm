39fbbf2b3aa4107674c0a36c473628bdf1d30d96
TanasieNicusor <tansie.cristian.g3c@student.ucv.ro>
2026-05-10 12:12:01 +0300

    asd

--- src/main/java/com/example/icsm/controller/AuthController.java
+++ src/main/java/com/example/icsm/controller/AuthController.java
@@ -1,21 +1,23 @@
 package com.example.icsm.controller;
 
-import com.example.icsm.model.User;
-import com.example.icsm.model.enums.UserRole;
-import com.example.icsm.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.icsm.model.User;
+import com.example.icsm.model.enums.UserRole;
+import com.example.icsm.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
-
+//a
     @GetMapping("/login")
     public String login() {
         return "auth/login";
